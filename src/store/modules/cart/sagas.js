b1d9f6c4ee973cdad51837ac555a912757affbe0
Matheus Loubake <mloubake@hotmail.com>
2021-02-08 15:30:06 -0300

    Getting Item Quantity

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,7 +1,16 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, takeLatest, select } from "redux-saga/effects";
 
-function checkProductStock() {
-  console.log("Added to the cart");
+function* checkProductStock(addProductToCart) {
+  const { product } = addProductToCart.payload;
+
+  const currentQuantity = yield select((state) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity);
 }
 
 export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
