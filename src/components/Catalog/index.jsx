1f3e1dd6ac7e1817ad3ca456959d3a358e5e2c01
Matheus Loubake <mloubake@hotmail.com>
2021-02-08 12:44:41 -0300

    Creating a server-json, Adding an api service, changing package.json, populating Catalog

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
+import api from "../../services/api";
+
 const Catalog = () => {
-  const catalog = useSelector((state) => state);
+  const [catalog, setCatalog] = useState([]);
 
-  console.log(catalog);
+  useEffect(() => {
+    api.get("products").then((response) => {
+      setCatalog(response.data);
+    });
+  }, []);
 
-  return <h1>Catalog</h1>;
+  return (
+    <>
+      <main>
+        <h1>Catalog</h1>
+
+        {catalog.map((product) => (
+          <article key={product.id}>
+            <strong>{product.title}</strong> {" - "}
+            <span>{product.price}</span> <button type="button">Buy</button>
+          </article>
+        ))}
+      </main>
+    </>
+  );
 };
 
 export default Catalog;
